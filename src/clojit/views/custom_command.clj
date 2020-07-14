c71166493e54c6647301bbcbcf05324e28c68954
Santiago Alvarez <alvarezsantiago96@gmail.com>
2020-07-14 10:22:28 +0200

    add keystrokes in menubar and 'execute command' view

--- src/clojit/views/custom_command.clj
+++ src/clojit/views/custom_command.clj
@@ -1,8 +1,20 @@
 (ns clojit.views.custom-command
-  (:import [javax.swing JTextField]))
+  (:require [clojit.git :as git])
+  (:import [javax.swing JTextField JTextArea JScrollPane])
+  (:import [java.awt BorderLayout])
+  (:import [java.awt.event ActionListener]))
 
 (defn execute-command-view
   "TODO"
-  [pane]
-  (let [output-text (JTextField.)
-        input (JTextField.)]))
+  [pane repo-path]
+  (let [output-text (JTextArea.)
+        input       (JTextField.)]
+    (.setEditable output-text false)
+    (.addActionListener input (reify ActionListener
+                                (actionPerformed [this _]
+                                  (.setText output-text (git/command repo-path (.getText input)))
+                                  (.setText input ""))))
+    (.setLayout pane (BorderLayout.))
+    (.add pane (JScrollPane. output-text))
+    (.add pane input BorderLayout/PAGE_END)
+    (.requestFocusInWindow input)))
--- src/clojit/views/menu.clj
+++ src/clojit/views/menu.clj
@@ -1,13 +1,15 @@
 (ns clojit.views.menu
   (:require [clojit.git :as git])
-  (:import [javax.swing JFileChooser JMenuItem JMenuBar JMenu])
-  (:import [java.awt.event ActionListener]))
+  (:import [javax.swing JFileChooser JMenuItem JMenuBar JMenu KeyStroke])
+  (:import [java.awt.event ActionListener KeyEvent]))
 
 (def repo-chooser (doto (JFileChooser.)
                     (.setFileSelectionMode JFileChooser/DIRECTORIES_ONLY)))
 
 (defn menu-item-open-repository [frame view-handler config-updater]
   (doto (JMenuItem. "Open Repository...")
+    ;; key bindings should be in config
+    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_O KeyEvent/CTRL_DOWN_MASK))
     (.addActionListener (reify ActionListener
                           (actionPerformed [this _]
                             (when (= (.showOpenDialog repo-chooser frame) JFileChooser/APPROVE_OPTION)
@@ -16,24 +18,32 @@
                                 (if (git/repository? repo-path)
                                   (do (config-updater :repository-path repo-path)
                                     (view-handler 'status))
+                                  ;; TODO: Make this a dialog
                                   (println "Directory " repo-path " is not a git repository!")))))))))
 
 (defn menu-item-update-status [view-handler]
-  (doto (JMenuItem. "Update status")
+  (doto (JMenuItem. "Status")
+    ;; key bindings should be in config
+    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_S KeyEvent/ALT_DOWN_MASK))
     (.addActionListener (reify ActionListener
                           (actionPerformed [this _]
                             (view-handler 'status))))))
 
 (defn menu-item-execute-command
   "TODO"
-  []
+  [view-handler]
   (doto (JMenuItem. "Execute command")
+    ;; key bindings should be in config
+    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_X KeyEvent/ALT_DOWN_MASK))
     (.addActionListener (reify ActionListener
-                          (actionPerformed [this _])))))
+                          (actionPerformed [this _]
+                            (view-handler 'execute-command))))))
 
 (defn commit
   [view-handler]
   (doto (JMenuItem. "Commit")
+    ;; key bindings should be in config
+    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_C KeyEvent/ALT_DOWN_MASK))
     (.addActionListener (reify ActionListener
                           ;; TODO: open dialog to input commit title and message
                           (actionPerformed [this _]
@@ -44,5 +54,5 @@
     (.add (doto (JMenu. "Menu")
             (.add (menu-item-open-repository frame view-handler config-updater))
             (.add (menu-item-update-status view-handler))
-            (.add (menu-item-execute-command))
+            (.add (menu-item-execute-command view-handler))
             (.add (commit view-handler))))))
