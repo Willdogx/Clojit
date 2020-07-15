dc9d9cd7a3c7ae2c994b53e336a606f560bee47a
Santiago Alvarez <alvarezsantiago96@gmail.com>
2020-07-15 18:29:01 +0200

    refactor menu

--- src/clojit/views/menu.clj
+++ src/clojit/views/menu.clj
@@ -1,58 +1,51 @@
 (ns clojit.views.menu
   (:require [clojit.git :as git])
-  (:import [javax.swing JFileChooser JMenuItem JMenuBar JMenu KeyStroke])
+  (:import [javax.swing JFileChooser JMenuItem JMenuBar JMenu KeyStroke JOptionPane])
   (:import [java.awt.event ActionListener KeyEvent]))
 
 (def repo-chooser (doto (JFileChooser.)
                     (.setFileSelectionMode JFileChooser/DIRECTORIES_ONLY)))
 
-(defn menu-item-open-repository [frame view-handler config-updater]
-  (doto (JMenuItem. "Open Repository...")
-    ;; key bindings should be in config
-    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_O KeyEvent/CTRL_DOWN_MASK))
-    (.addActionListener (reify ActionListener
-                          (actionPerformed [this _]
-                            (when (= (.showOpenDialog repo-chooser frame) JFileChooser/APPROVE_OPTION)
-                              ; Save the path of the repo in a config file and initialize git commands
-                              (let [repo-path (-> repo-chooser (.getSelectedFile) (.getAbsolutePath))]
-                                (if (git/repository? repo-path)
-                                  (do (config-updater :repository-path repo-path)
-                                    (view-handler 'status))
-                                  ;; TODO: Make this a dialog
-                                  (println "Directory " repo-path " is not a git repository!")))))))))
+(defn menuitem
+  [name & {:keys [keys on-click]}]
+  (let [item (JMenuItem. name)]
+    (when keys
+      ;; TODO: parse keys from string. E.g. "ALT-X" or "CTRL-O" or "C-o"
+      (.setAccelerator item (apply #(KeyStroke/getKeyStroke %1 %2) keys)))
+    (when on-click
+      (.addActionListener item (reify ActionListener
+                                 (actionPerformed [this _]
+                                   (on-click)))))
+    item))
 
-(defn menu-item-update-status [view-handler]
-  (doto (JMenuItem. "Status")
-    ;; key bindings should be in config
-    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_S KeyEvent/ALT_DOWN_MASK))
-    (.addActionListener (reify ActionListener
-                          (actionPerformed [this _]
-                            (view-handler 'status))))))
-
-(defn menu-item-execute-command
-  "TODO"
-  [view-handler]
-  (doto (JMenuItem. "Execute command")
-    ;; key bindings should be in config
-    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_X KeyEvent/ALT_DOWN_MASK))
-    (.addActionListener (reify ActionListener
-                          (actionPerformed [this _]
-                            (view-handler 'execute-command))))))
-
-(defn commit
-  [view-handler]
-  (doto (JMenuItem. "Commit")
-    ;; key bindings should be in config
-    (.setAccelerator (KeyStroke/getKeyStroke KeyEvent/VK_C KeyEvent/ALT_DOWN_MASK))
-    (.addActionListener (reify ActionListener
-                          ;; TODO: open dialog to input commit title and message
-                          (actionPerformed [this _]
-                            (view-handler 'commit))))))
+(defn menubar
+  [name & items]
+  (let [menubar (JMenuBar.)
+        menu (JMenu. name)]
+    (doseq [{:keys [name keys on-click]} items]
+      (.add menu (menuitem name :keys keys :on-click on-click)))
+    (doto menubar (.add menu))))
 
 (defn menu-bar [frame view-handler config-updater]
-  (doto (JMenuBar.)
-    (.add (doto (JMenu. "Menu")
-            (.add (menu-item-open-repository frame view-handler config-updater))
-            (.add (menu-item-update-status view-handler))
-            (.add (menu-item-execute-command view-handler))
-            (.add (commit view-handler))))))
+  (menubar "Menu"
+    {:name "Open Repository..."
+     :keys [KeyEvent/VK_O KeyEvent/CTRL_DOWN_MASK]
+     :on-click (fn []
+                       (when (= (.showOpenDialog repo-chooser frame) JFileChooser/APPROVE_OPTION)
+                              ; Save the path of the repo in a config file and initialize git commands
+                         (let [repo-path (-> repo-chooser (.getSelectedFile) (.getAbsolutePath))]
+                           (if (git/repository? repo-path)
+                             (do (config-updater :repository-path repo-path)
+                               (view-handler 'status))
+                             (JOptionPane/showMessageDialog
+                               frame
+                               (str "Directory " repo-path " is not a git repository!"))))))}
+    {:name "Status"
+     :keys [KeyEvent/VK_S KeyEvent/ALT_DOWN_MASK]
+     :on-click #(view-handler 'status)}
+    {:name "Execute Command"
+     :keys [KeyEvent/VK_X KeyEvent/ALT_DOWN_MASK]
+     :on-click #(view-handler 'execute-command)}
+    {:name "Commit"
+     :keys [KeyEvent/VK_C KeyEvent/ALT_DOWN_MASK]
+     :on-click #(view-handler 'commit)}))
